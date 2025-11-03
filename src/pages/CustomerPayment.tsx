a2b4b4bf84fe973dee831106ad7909ec45edd2ac
Kayode Marvellous <marvellouskayode17@gmail.com>
2025-11-03 13:12:13 +0100

    I don tire for this Celo Chain ID matter

--- src/pages/CustomerPayment.tsx
+++ src/pages/CustomerPayment.tsx
@@ -190,10 +190,10 @@ const CustomerPayment = () => {
         
         // Check network
         const network = await provider.send("eth_chainId",[]);
-          if (network !== "11142220") {
+          if (network !== "44787") {
             toast({
               title: "Wrong Network",
-              description: "Please switch to Celo Sopelia Testnet to continue.",
+              description: "Please switch to Celo Alfajores Testnet to continue.",
               variant: "destructive"
             });
             return
