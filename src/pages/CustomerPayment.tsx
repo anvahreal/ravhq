d9840e60f290c82dcaf920291415a1d33b0d04c8
Kayode Marvellous <marvellouskayode17@gmail.com>
2025-11-05 10:17:36 +0100

    This yeye celo sopelia supposed work

--- src/pages/CustomerPayment.tsx
+++ src/pages/CustomerPayment.tsx
@@ -201,11 +201,11 @@ const CustomerPayment = () => {
 
         // Switch to Celo network
     try {
-      await provider.send("wallet_switchEthereumChain", [{ chainId: "0xaa254c" }]); // Sepolia testnet
+      await provider.send("wallet_switchEthereumChain", [{ chainId: "0xA9F548" }]); // Sepolia testnet
     } catch (switchError: any) {
       if (switchError.code === 4902) {
         await provider.send("wallet_addEthereumChain", [{
-          chainId: "0xaa254c",
+          chainId: "0xA9F548",
           chainName: "Celo Testnet",
           nativeCurrency: { name: "CELO SOPELIA", symbol: "CELO", decimals: 18 },
           rpcUrls: ["https://forno.celo-sepolia.celo-testnet.org"], // testnet RPC
